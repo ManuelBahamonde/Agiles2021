13db00683f9bc23ab55fd562a098293b4e13b86c
ManuelBahamonde <manuelbahamondedev@gmail.com>
2022-02-21 17:57:31 -0300

    Fixed some of the SonarQube code smells/issues

--- Tp.UI/Program.cs
+++ Tp.UI/Program.cs
@@ -4,19 +4,6 @@ namespace Tp.UI
 {
     static class Program
     {
-        static void Main(string[] args)
-        {
-            // var name = InputName();
-        }
-
-        static string InputName()
-        {
-            Console.WriteLine("Ingrese su nombre");
-            var name = Console.ReadLine();
-
-            // Utilities.ValidateName(name); TODO
-
-            return name;
-        }
+        static void Main(string[] args) { }
     }
 }
